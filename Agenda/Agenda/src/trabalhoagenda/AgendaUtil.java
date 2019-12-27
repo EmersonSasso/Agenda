a678b8a69166e5fb74685787fac4586c13f231f2
EmersonSasso <59214382+EmersonSasso@users.noreply.github.com>
2019-12-27 14:32:50 -0300

    Add files via upload
    Um exemplo de uma agenda em java com o banco Postgres

---
+++ Agenda/Agenda/src/trabalhoagenda/AgendaUtil.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhoagenda;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Usuário
+ */
+public abstract class AgendaUtil {
+
+    private boolean aux;
+
+    public String ValidaEmail(String Email) throws Exception {
+        String regex = "^(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(Email);
+        if (matcher.matches()) {
+            return Email;
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public String ValidaTelefone(String DDD, String Telefone, int tipo) throws NumberFormatException {
+        aux = true;
+
+        Integer.parseInt(DDD);
+        if (DDD.length() != 2) {
+            throw new NumberFormatException();
+        };
+        Integer.parseInt(Telefone);
+        if (!(Telefone.length() == 9 || Telefone.length() == 8)) {
+            throw new NumberFormatException();
+        };
+        return DDD + "#" + Telefone;
+    }
+
+}
